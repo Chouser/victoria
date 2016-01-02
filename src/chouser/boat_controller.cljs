d7f9106294ee20a79d77f9de159f94eed1ac44c9
Chouser <chouser@n01se.net>
2016-01-02 17:56:14 -0500

    Map and clamp client coords to independant scale.

--- src/chouser/boat_controller.cljs
+++ src/chouser/boat_controller.cljs
@@ -14,12 +14,41 @@
                   (fn [e]
                     (js/console.log "Response" (.getResponseText (.-target e)))))))
 
+(def maxpos 1000)
+(def ctrl-scale 2.0)
 (def ctrls (atom {:ids {} :left {:pos 0} :right {:pos 0}}))
 
 (defn xy [touch]
   [(.-clientX touch) (.-clientY touch)])
 
+(defn collater []
+  (let [id (atom :none)]
+    (with-meta (fn [schedule execute]
+                 (when (= :none @id)
+                   (reset! id (schedule (fn []
+                                          (reset! id :none)
+                                          (execute))))))
+      {:id id})))
+
+(def schedule-draw (collater))
+(def schedule-send (collater))
+
+(defn dirty! [ctrl-key]
+  (schedule-draw #(js/requestAnimationFrame %)
+                 #(doseq [[ctrl-key dom-id] {:left "sails", :right "rudder"}]
+                    (set! (-> (dom/getElement dom-id) .-style .-top)
+                          (str (* 0.08 (-> @ctrls ctrl-key :pos)) "%"))))
+
+  (schedule-send #(js/setTimeout % 1000)
+                 #(net/transmit xhr
+                                (str js/location "ctrl") "PUT"
+                                (-> {:sails (-> @ctrls :left :pos)
+                                     :rudder (-> @ctrls :right :pos)}
+                                    clj->js json/serialize))))
+
 (defn touchstart [event]
+  (.preventDefault event)
+  (.webkitRequestFullScreen js/document.documentElement)
   (let [container (dom/getElement "main")
         width (.-clientWidth container)
         height (.-clientHeight container)]
@@ -40,59 +69,32 @@
         (when ctrl-key
           (swap! ctrls
                  (fn [ctrls]
-                   (let [pos (-> ctrls ctrl-key :pos)]
-                     (prn (- y pos))
+                   (let [old-pos (-> ctrls ctrl-key :pos)]
                      (-> ctrls
-                         (assoc ctrl-key {:pos pos
-                                          :drag-offset (- pos y)
+                         (assoc ctrl-key {:pos old-pos
+                                          :drag-offset (- (* height (/ old-pos maxpos ctrl-scale)) y)
                                           :x x})
                          (assoc-in [:ids (.-identifier touch)] ctrl-key)))))
           #_(prn ctrl-key @ctrls))))))
 
-(defn collater []
-  (let [id (atom :none)]
-    (with-meta (fn [schedule execute]
-                 (when (= :none @id)
-                   (reset! id (schedule (fn []
-                                          (reset! id :none)
-                                          (execute))))))
-      {:id id})))
-
-(def schedule-draw (collater))
-(def schedule-send (collater))
-
-(defn dirty! [ctrl-key]
-  (schedule-draw #(js/requestAnimationFrame %)
-                 #(doseq [[ctrl-key dom-id] {:left "sails", :right "rudder"}]
-                    (set! (-> (dom/getElement dom-id) .-style .-top)
-                          (str (-> @ctrls ctrl-key :pos) "px"))))
-
-  (schedule-send #(js/setTimeout % 1000)
-                 #(net/transmit xhr
-                                (str js/location "ctrl") "PUT"
-                                (-> {:sails (-> @ctrls :left :pos)
-                                     :rudder (-> @ctrls :right :pos)}
-                                    clj->js json/serialize))))
-
-(defn touchmove [event]
+(defn touchupdate [type event]
   (doseq [touch (prim-seq (.-changedTouches (.-event_ event)))]
     (let [ctrl-key (get-in @ctrls [:ids (.-identifier touch)])
+          height (.-clientHeight (dom/getElement "main"))
           [x y] (xy touch)]
       (swap! ctrls update ctrl-key
              (fn [ctrl]
-               (assoc ctrl
-                 :pos (+ y (:drag-offset ctrl))
-                 :x x)))
-      (dirty! ctrl-key))))
+               (let [pos (Math/round (* ctrl-scale (/ maxpos height) (+ y (:drag-offset ctrl))))]
+                 (-> (if (= type :end)
+                       (dissoc ctrl :x)
+                       (assoc ctrl :x x))
+                     (merge (cond
+                             (< pos 0) {:pos 0, :drag-offset (- y)}
+                             (> pos maxpos) {:pos maxpos, :drag-offset (- (/ height ctrl-scale) y)}
+                             :else {:pos pos}))))))
 
-(defn touchend [event]
-  (doseq [touch (prim-seq (.-changedTouches (.-event_ event)))]
-    (let [ctrl-key (get-in @ctrls [:ids (.-identifier touch)])
-          [x y] (xy touch)]
-      (swap! ctrls update ctrl-key
-             (fn [ctrl]
-               {:pos (+ y (:drag-offset ctrl))}))
-      (swap! ctrls update :ids dissoc ctrl-key)
+      (when (= type :end)
+        (swap! ctrls update :ids dissoc ctrl-key))
       (dirty! ctrl-key))))
 
 (defn load []
@@ -107,8 +109,8 @@
     (dom/append main-elem (dom/createDom "div" (js-obj "id" "rudder" "class" "ctrl")))
 
     (events/listen main-elem "touchstart" touchstart)
-    (events/listen main-elem "touchmove" touchmove)
-    (events/listen main-elem "touchend" touchend))
+    (events/listen main-elem "touchmove" (partial touchupdate :move))
+    (events/listen main-elem "touchend"  (partial touchupdate :end)))
 
   (js/console.log "Load complete"))
 
